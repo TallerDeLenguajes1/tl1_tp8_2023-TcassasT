8570036f0a7ccc000216299bac6e7afedcb6f0c3
Tomás Cassas <tcassas@archt.com.ar>
2023-06-08 19:02:27 -0300

    Mover tareas pendientes a lista de tareas finailzadas con menu, control tareas pendientes

--- Program.cs
+++ Program.cs
@@ -14,12 +14,65 @@ internal class Program {
     for(int i = 0; i < cantidadDeTareas; i++) {
       listaTareas.Add(crearTareaNueva(i));
     }
-    
-    
+
+    List<Tarea> tareasFinalizadas = new List<Tarea>();
+    int decision = mostrarMenuYPedirOpcion();
+    while (decision != 3) {
+      switch(decision) {
+        case 1:
+          if (listaTareas.Count() == 0) {
+            Console.WriteLine("\n¡No hay tareas pendientes!");
+            break;
+          }
+
+          Tarea tareaAMover = moverTareaAFinalizadas(listaTareas);
+          tareasFinalizadas.Add(tareaAMover);
+          listaTareas.Remove(tareaAMover);
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+
+      decision = mostrarMenuYPedirOpcion();
+    }
   }
 
-  static int mostrarMenuYPedirOpcion(Boolean volverAPedir) {
-    Console.WriteLine("Ingrese una opción para operar con las tareas ingresadas:");
+  static Tarea moverTareaAFinalizadas(List<Tarea> tareasPendientes) {
+    Console.WriteLine("\nListado de tareas (" + tareasPendientes.Count() + "): ");
+
+    foreach (var tarea in tareasPendientes) {
+      mostrarTarea(tarea);
+    }
+
+    int tareaIdAMover;
+    Console.Write("\nIngrese el ID de la tarea que desea mover a finalizadas:");
+
+    if (!int.TryParse(Console.ReadLine(), out tareaIdAMover)) {
+      Console.WriteLine("x Input invalido, intente nuevamente.");
+      return moverTareaAFinalizadas(tareasPendientes);
+    }
+
+    int tareaListIndex = 0;
+    Boolean tareaExistente = false;
+    for (int i = 0; i < tareasPendientes.Count(); i++) {
+      if (tareasPendientes[i].TareaId == tareaIdAMover) {
+        tareaListIndex = i;
+        tareaExistente = true;
+      }
+    }
+
+    if (!tareaExistente) {
+      Console.WriteLine("x Tarea inexistente, intente nuevamente.");
+      return moverTareaAFinalizadas(tareasPendientes);
+    }
+
+    return tareasPendientes[tareaListIndex];
+  }
+
+  static int mostrarMenuYPedirOpcion() {
+    Console.WriteLine("\nIngrese una opción para operar con las tareas ingresadas:");
     Console.WriteLine(" 1- Mover tarea pendiente a realizada");
     Console.WriteLine(" 2- Buscar tarea por descripcion");
     Console.WriteLine(" 3- Salir");
@@ -27,18 +80,18 @@ internal class Program {
     int decision;
     if (int.TryParse(Console.ReadLine(), out decision)) {
       if (decision < 1 || decision > 3) {
-        Console.WriteLine("Opción invalida, ingrese una opción para operar con las tareas ingresadas:");
-        return mostrarMenuYPedirOpcion(true);
+        Console.WriteLine("x Opción invalida, intente nuevamente.");
+        return mostrarMenuYPedirOpcion();
       }
       return decision;
     } else {
-      Console.WriteLine("Opción invalida, ingrese una opción para operar con las tareas ingresadas:");
-      return mostrarMenuYPedirOpcion(true);
+      Console.WriteLine("x Opción invalida, intente nuevamente.");
+      return mostrarMenuYPedirOpcion();
     }
   }
 
   static Tarea crearTareaNueva(int tareaId) {
-    Console.WriteLine("- Creando tarea nueva N° (" + tareaId + ")");
+    Console.WriteLine("\n- Creando tarea nueva N° (" + tareaId + ")");
     string descripcion = solicitaDescripcion();
     int duracion = solicitaDuracion();
     return new Tarea(tareaId, descripcion, duracion);
@@ -69,7 +122,7 @@ internal class Program {
   }
 
   static void mostrarTarea(Tarea tarea) {
-    Console.WriteLine("- Tarea N° " + tarea.TareaId);
+    Console.WriteLine("\n- Tarea N° " + tarea.TareaId);
     Console.WriteLine(" Duracion: " + tarea.Duracion);
     Console.WriteLine(" Descripción: " + tarea.Descripcion);
   }
